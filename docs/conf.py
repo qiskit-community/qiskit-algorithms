e80bbbc2a156c41c91fc7081351da4f052384f1e
Melech Lapson <Melech.Lapson@ibm.com>
2024-04-16 15:50:09 -0500

    reformat file

--- docs/conf.py
+++ docs/conf.py
@@ -231,7 +231,9 @@ def linkcode_resolve(domain, info):
         return None
     try:
         relative_file_name = Path(full_file_name).resolve().relative_to(REPO_ROOT)
-        file_name = re.sub(r"\.tox\/.+\/site-packages\/", "", str(relative_file_name))
+        file_name = re.sub(r"\.tox\/.+\/site-packages\/",
+                           "",
+                           str(relative_file_name))
     except ValueError:
         return None
 
