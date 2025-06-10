2eac1eb81cc955fb9bcb7679ad1b14b5166e8fc0
Tristan NEMOZ <tristan.nemoz@gmail.com>
2025-06-10 23:28:21 +0200

    Changed copyright of adaptvqe

--- qiskit_algorithms/minimum_eigensolvers/adapt_vqe.py
+++ qiskit_algorithms/minimum_eigensolvers/adapt_vqe.py
@@ -1,6 +1,6 @@
 # This code is part of a Qiskit project.
 #
-# (C) Copyright IBM 2022, 2024.
+# (C) Copyright IBM 2022, 2025.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -160,7 +160,8 @@ class AdaptVQE(VariationalAlgorithm, MinimumEigensolver):
         # The excitations operators are applied later as exp(i*theta*excitation).
         # For this commutator, we need to explicitly pull in the imaginary phase.
         commutators = [1j * (operator @ exc - exc @ operator) for exc in self._excitation_pool]
-        # We have to call simplify on it since Qiskit doesn't do so from 2.1 onward
+        # We have to call simplify on it since Qiskit doesn't do so from 2.1 onward, see
+        # Qiskit/qiskit/issues/14567
         commutators = [obs.simplify() for obs in commutators]
         res = estimate_observables(self.solver.estimator, self.solver.ansatz, commutators, theta)
         return res
