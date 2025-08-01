5bf86410be73ebebd47eb751c7933a4452f313a9
Tristan NEMOZ <tristan.nemoz@gmail.com>
2025-08-02 01:45:40 +0200

    Handled nil observables in observables_evaluators.py

--- qiskit_algorithms/observables_evaluator.py
+++ qiskit_algorithms/observables_evaluator.py
@@ -19,8 +19,8 @@ from typing import Any
 import numpy as np
 
 from qiskit import QuantumCircuit
-from qiskit.quantum_info import SparsePauliOp
 from qiskit.primitives import BaseEstimatorV2
+from qiskit.quantum_info import SparsePauliOp
 from qiskit.quantum_info.operators.base_operator import BaseOperator
 
 from .exceptions import AlgorithmError
@@ -63,8 +63,8 @@ def estimate_observables(
         observables_list = observables
 
     if len(observables_list) > 0:
-        observables_list = _handle_zero_ops(observables_list)
-        parameter_values_: Sequence[float] | Sequence[Sequence[float]] | None = parameter_values
+        observables_list, mask = _handle_zero_ops(observables_list)
+        parameter_values_: Sequence[float] | None = parameter_values
         try:
             estimator_job = estimator.run([(quantum_state, observables_list, parameter_values_)])
             estimator_result = estimator_job.result()[0]
@@ -75,8 +75,18 @@ def estimate_observables(
         metadata = estimator_result.metadata
         # Discard values below threshold
         observables_means = expectation_values * (np.abs(expectation_values) > threshold)
+        # Adding back the values from the nil observables
+        all_means = []
+        index = 0
+        for mask_value in mask:
+            # Non-zero observable
+            if mask_value:
+                all_means.append(observables_means[index])
+                index += 1
+            else:
+                all_means.append(0.0)
         # zip means and metadata into tuples
-        observables_results = list(zip(observables_means, [metadata] * len(observables_means)))
+        observables_results = list(zip(np.array(all_means), [metadata] * len(all_means)))
     else:
         observables_results = []
 
@@ -85,15 +95,23 @@ def estimate_observables(
 
 def _handle_zero_ops(
     observables_list: list[BaseOperator],
-) -> list[BaseOperator]:
-    """Replaces all occurrence of operators equal to 0 in the list with an equivalent ``SparsePauliOp``
-    operator."""
+) -> tuple[list[BaseOperator], list[bool]]:
+    """Returns the initial list of operators with the nil ones having been removed. Furthermore, returns
+    a mask indicating which operators were kept."""
+    mask: list[bool] = []
+    purged_list: list[BaseOperator] = []
+
     if observables_list:
-        zero_op = SparsePauliOp.from_list([("I" * observables_list[0].num_qubits, 0)])
-        for ind, observable in enumerate(observables_list):
-            if observable == 0:
-                observables_list[ind] = zero_op
-    return observables_list
+        for observable in observables_list:
+            # TODO: should consider Pauli objects too
+            if observable == 0 or (
+                isinstance(observable, SparsePauliOp) and not observable.simplify().coeffs.any()
+            ):
+                mask.append(False)
+            else:
+                mask.append(True)
+                purged_list.append(observable)
+    return purged_list, mask
 
 
 def _prepare_result(
